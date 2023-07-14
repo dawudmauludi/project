35aa64a381db5f42ddd48664f754eab21913ccbb
dawud mauludi <dawudmauludixrpl@gmail.com>
2023-07-14 12:46:35 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const navbarNav = document.querySelector('#hamburger-menu');
+
+const nav = document.querySelector('nav ul');
+
+navbarNav.addEventListener('click', function() {
+    nav.classList.toggle('active')
+})
+
+
+
+
